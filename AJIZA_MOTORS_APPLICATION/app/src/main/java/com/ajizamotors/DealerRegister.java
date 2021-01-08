222ffb692bda9f45c861c161f02471c2a6ca0f0c
jaffarabbas <gamaportal8@gmail.com>
2021-01-08 15:38:34 +0500

    1-8-2021        Dealer Register

--- AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/DealerRegister.java
+++ AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/DealerRegister.java
@@ -28,9 +28,8 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 public class DealerRegister extends AppCompatActivity {
     EditText DealerRegisterUsername,DealerRegisterPassword;
-    Button DealerRegister,GoogleRegister;
+    Button DealerRegister;
     ImageButton Back;
-    GoogleSignInClient googleSignInClient;
     FirebaseAuth firebaseAuth;
 
     @Override
@@ -42,7 +41,6 @@ public class DealerRegister extends AppCompatActivity {
         DealerRegisterUsername = findViewById(R.id.DealerRegesterUsername);
         DealerRegisterPassword = findViewById(R.id.DealerUserPassword);
         DealerRegister = findViewById(R.id.DealerRegesterButton);
-        GoogleRegister = findViewById(R.id.DealerRegisterGoogleAuth);
         Back = findViewById(R.id.BackToDealerLogin);
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -67,73 +65,6 @@ public class DealerRegister extends AppCompatActivity {
                 }
             }
         });
-
-
-        final GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("322105425453-99isbl317rblqnlkcdlgl5a1ol6rff6i.apps.googleusercontent.com").requestEmail().build();
-
-        googleSignInClient = GoogleSignIn.getClient(DealerRegister.this, googleSignInOptions);
-
-        GoogleRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-                Intent intent = googleSignInClient.getSignInIntent();
-                startActivityForResult(intent, 9001);
-            }
-        });
-        firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-
-        if (firebaseUser != null){
-            startActivity(new Intent(DealerRegister.this, Dashboard.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-        }
-
-
-        Back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(DealerRegister.this,LoginPage.class));
-            }
-        });
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 9001){
-            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-            if (signInAccountTask.isSuccessful()){
-                String s= "Google Sign in successful!";
-                displayToast(s);
-
-                try {
-                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
-
-                    AuthCredential authCredential = null;
-                    if (googleSignInAccount != null) {
-                        authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
-                    }
-                    firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-                                startActivity(new Intent(DealerRegister.this, Dashboard.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
-                                displayToast("Firebase authentication is successful");
-                            }
-                            else {
-                                displayToast("Authentication Failed : " + task.getException().getMessage());
-                            }
-                        }
-                    });
-                }
-                catch (ApiException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 
     //Register from mail
