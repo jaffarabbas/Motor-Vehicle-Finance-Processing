2f03e726b6aa98d81a492a61c47edf19b0161e19
jaffarabbas <gamaportal8@gmail.com>
2021-01-14 22:33:53 +0500

    1-14-2021     Login System For Dealer

--- AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/DealerLogin.java
+++ AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/DealerLogin.java
@@ -4,19 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class DealerLogin extends AppCompatActivity {
     private final int RC_SIGN_IN = 0;
     EditText DealerRegisterUsername,DealerRegisterPassword;
-    Button DealerGoogleLogin,DealerLogin,DealerRegistration;
+    Button DealerLogin,DealerRegistration;
     ImageButton Back;
+    SignInButton signInButton;
+    GoogleSignInClient googleSignInClient;
     FirebaseAuth LoginAuth;
 
     @Override
@@ -30,7 +40,7 @@ public class DealerLogin extends AppCompatActivity {
         DealerLogin = findViewById(R.id.DealerLogin);
         Back = findViewById(R.id.BackToFirstPage);
         DealerRegistration = findViewById(R.id.IntoDealerRegistration);
-
+        signInButton = findViewById(R.id.GoogleLoginDealer);
         //Login
         this.DealerLogin.setOnClickListener(new View.OnClickListener() {
             String user,pass;
@@ -49,7 +59,23 @@ public class DealerLogin extends AppCompatActivity {
 
         //Google Sign in
 
+        //Google Sign in
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.GoogleLoginDealer:
+                        signIn();
+                        break;
+                }
+            }
+        });
 
 
         //Registration
@@ -68,11 +94,47 @@ public class DealerLogin extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            startActivity(new Intent(DealerLogin.this, Dashboard.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            displayToast("Login IN");
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Error", "signInResult:failed code=" + e.getStatusCode());
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
     //Login
     private void LoginUser(String name,String password) {
         LoginAuth.signInWithEmailAndPassword(name, password).addOnSuccessListener(DealerLogin.this, authResult -> {
             Toast.makeText(DealerLogin.this,"Login Successful",Toast.LENGTH_LONG).show();
             startActivity(new Intent(DealerLogin.this, Dashboard.class));
         });
+    }
+
+    private void displayToast(String s) {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
     }
 }
