a5906ad5a5eff31392c7295e3282839f2a129ba8
jaffarabbas <gamaportal8@gmail.com>
2021-01-15 12:40:33 +0500

    1-15-2021    AdminLogin Update

--- AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/AdminLogin.java
+++ AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/AdminLogin.java
@@ -2,13 +2,26 @@ package com.ajizamotors;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminLogin extends AppCompatActivity {
-
+    Button newAdmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+
+        newAdmin = findViewById(R.id.NewAdmin);
+
+
+        newAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminLogin.this,AdminRegister.class));
+            }
+        });
     }
 }
