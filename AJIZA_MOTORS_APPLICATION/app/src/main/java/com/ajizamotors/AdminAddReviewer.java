8987cef0eb5f1862504991a9de3dae21a8ceb9b7
jaffarabbas <gamaportal8@gmail.com>
2021-01-15 21:02:40 +0500

    1-15-2021        Update AdminAddReviewer

--- AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/AdminAddReviewer.java
+++ AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/AdminAddReviewer.java
@@ -16,11 +16,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+
 public class AdminAddReviewer extends AppCompatActivity {
     RecyclerView recyclerView;
     FirebaseDatabase firebaseDatabase =  FirebaseDatabase.getInstance();;
     DatabaseReference databaseReference;
-
+    AdminReviewAddViewHolder adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,36 +30,29 @@ public class AdminAddReviewer extends AppCompatActivity {
         recyclerView = findViewById(R.id.AdminCreateAddReviewView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        databaseReference = firebaseDatabase.getReference("CreateAdd");
+        databaseReference = firebaseDatabase.getReference().child("CreateAdd");
         loadData();
     }
 
-    private void loadData() {
-        FirebaseRecyclerOptions<Dataload> options =
-                new FirebaseRecyclerOptions.Builder<Dataload>().setQuery(databaseReference,Dataload.class).build();
-        FirebaseRecyclerAdapter<Dataload, AdminReviewAddViewHolder> firebaseRecyclerAdapter =
-                new FirebaseRecyclerAdapter<Dataload,AdminReviewAddViewHolder>(options) {
-                    @Override
-                    protected void onBindViewHolder(@NonNull AdminReviewAddViewHolder holder, int position, @NonNull Dataload model) {
-                     //   Dataload obj = new Dataload(model.getCarAddress(),model.getCarModel(),model.getCarName(),model.getCarPayment(),model.getCarPrice(),model.getCarImage(),model.getCarSellerName(),model.getCarSellerContact());
-                        holder.ClassCarFeedName.setText(model.getName());
-                        holder.ClassCarFeedModel.setText(model.getModel());
-                        holder.ClassCarFeedPrice.setText(model.getPrice());
-                        holder.ClassCarFeedPayment.setText(model.getPayment());
-                        holder.ClassCarFeedAddress.setText(model.getAddress());
-                        holder.ClassCarFeedSellerName.setText(model.getSellerName());
-                        holder.ClassCarFeedSellerContact.setText(model.getSellerContact());
-                        Picasso.get().load(model.getImage()).into(holder.ClassFeedImage);
-                    }
 
-                    @NonNull
-                    @Override
-                    public AdminReviewAddViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.createaddreviewfeed,parent,false);
-                        return new AdminReviewAddViewHolder(view);
-                    }
-                };
-        firebaseRecyclerAdapter.startListening();
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    private void loadData() {
+        FirebaseRecyclerOptions<Load> options =
+                new FirebaseRecyclerOptions.Builder<Load>().setQuery(databaseReference,Load.class).build();
+        adapter = new AdminReviewAddViewHolder(options);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
