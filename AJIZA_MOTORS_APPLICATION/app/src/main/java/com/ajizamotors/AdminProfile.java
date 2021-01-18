a9517bf17fef54e6da80f887b6787d65c5c21145
jaffarabbas <gamaportal8@gmail.com>
2021-01-19 04:58:14 +0500

    1-19-2021  Admin Profile Saver

---
+++ AJIZA_MOTORS_APPLICATION/app/src/main/java/com/ajizamotors/AdminProfile.java
@@ -0,0 +1,48 @@
+package com.ajizamotors;
+
+public class AdminProfile {
+    String email,imageUrl,name,password;
+
+    public AdminProfile(){
+
+    }
+
+    public AdminProfile(String email, String imageUrl, String name, String password) {
+        this.email = email;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
